acfb1dc138c066af098d7d20cd0a78a4bf0984d9
dmnunez1993 <dmnunez1993@gmail.com>
2023-03-08 14:25:05 +0000

    implementated functions without debug

--- backup_handlers.py
+++ backup_handlers.py
@@ -1,5 +1,28 @@
+from datetime import datetime
 import os
 
+from process import run_command
+
+
 class MySQLBackupHandler(object):
+
+    def __init__(self):
+        self._database_host = os.environ['MYSQL_HOST']
+        self._database_port = os.environ['MYSQL_PORT']
+        self._database_user = os.environ['MYSQL_USER']
+        self._database_password = os.environ['MYSQL_PASSWORD']
+        self._database_name = os.environ['MYSQL_DATABASE_NAME']
+        self._output_folder = os.environ['OUTPUT_FOLDER']
+
     def backup(self):
-        pass
+        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S')
+        target_filename = f'{self._database_name}-{timestamp}.sql'
+        output_path = os.path.join(self._output_folder, target_filename)
+        command = f"""
+            mysql \
+                --host={self._database_host} \
+                --port={self._database_port} \
+                --user={self._database_user} \
+                --password='{self._database_password}' > {output_path}
+            """
+        run_command(command)
--- run_backup_scheduler.py
+++ run_backup_scheduler.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python
+import os
+
+from dotenv import load_dotenv
+
+from db_types import DATABASE_TYPE_MYSQL
+from backup_handlers import MySQLBackupHandler
+
+if os.path.isfile(".env"):
+    load_dotenv(os.path.join(".env"))
+
+DATABASE_TYPE = os.environ['DATABASE_TYPE']
+
+
+def main():
+    if DATABASE_TYPE == DATABASE_TYPE_MYSQL:
+        print("here")
+        handler = MySQLBackupHandler()
+        handler.backup()
+
+
+if __name__ == '__main__':
+    main()
