9fdf92fd90c787dbe3f4f853feaea5cb69c56dda
dmnunez1993 <dmnunez1993@gmail.com>
2023-03-08 15:55:25 +0000

    add google drive calls to scheduler

--- backup_scheduler.py
+++ backup_scheduler.py
@@ -6,6 +6,7 @@ from apscheduler.triggers.cron import CronTrigger
 
 from db_types import DATABASE_TYPE_MYSQL
 from backup_handlers import MySQLBackupHandler
+from google_drive_handler import GoogleDriveHandler
 
 
 class BackupScheduler(object):
@@ -19,13 +20,25 @@ class BackupScheduler(object):
         self._scheduler.add_job(self.handle_backups,
                                 CronTrigger.from_crontab(self._schedule))
 
+        self._google_drive_backup_enabled = os.environ.get(
+            'GOOGLE_DRIVE_BACKUP_ENABLED', 'true') == 'true'
+
     def handle_backups(self):
         if self._database_type == DATABASE_TYPE_MYSQL:
             handler = MySQLBackupHandler(logger=self._logger)
-            handler.backup()
+            backup_filename = handler.backup()
             limit_dt = datetime.utcnow() - timedelta(
                 days=self._days_to_keep_backups)
-            handler.clean_backups_before_time(limit_dt)
+            removed_filenames = handler.clean_backups_before_time(limit_dt)
+
+            if self._google_drive_backup_enabled:
+                self._handle_google_drive_backups(backup_filename,
+                                                  removed_filenames)
+
+    def _handle_google_drive_backups(self, backup_filename, removed_filenames):
+        gdrive_handler = GoogleDriveHandler()
+        gdrive_handler.upload_file(backup_filename)
+        gdrive_handler.remove_files(removed_filenames)
 
     def run(self):
         self._scheduler.start()
